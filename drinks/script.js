45700053b89abe286d2db72b189b6be456c82f1f
Jeffrey.Lee <v1001001@users.noreply.github.com>
2019-05-24 20:31:30 +0800

    赞赏链接改成自己的

--- drinks/script.js
+++ drinks/script.js
@@ -11,7 +11,7 @@ var drinks_qrcodes = $('#drinks-qrcodes');
 var drinks_qrcode = $('#drinks-qrcode');
 var isMobile = /Android|webOS|iPhone|iPad|BlackBerry/i.test(navigator.userAgent);
 
-var GithubLink  =   "https://github.com/Kaiyuan/donate-page/";
+var GithubLink  =   "https://github.com/v1001001/donate-page/";
 var PayPalLink  =   "https://www.paypal.me/KaiyuanXie";
 $('#paypal-donate>a').href = PayPalLink;
 $('#github-box>a').href = GithubLink;
